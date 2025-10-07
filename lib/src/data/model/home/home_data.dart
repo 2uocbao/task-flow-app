62365a1ee1c2b03258cf783a21df53116a9d4c2c
2uocbao <2uocbao.it@gmail.com>
2025-10-07 23:25:04 +0700

    Update home data

--- lib/src/data/model/home/home_data.dart
+++ lib/src/data/model/home/home_data.dart
@@ -3,30 +3,27 @@ import 'package:taskflow/src/data/model/task/task_data.dart';
 import 'package:taskflow/src/data/model/team/team_data.dart';
 
 class HomeData {
-  List<TeamData>? teamData;
-  List<StatusSummary>? statusSummary;
-  List<TaskData>? taskData;
+  List<TeamData>? teamData = <TeamData>[];
+  List<StatusSummary>? statusSummary = <StatusSummary>[];
+  List<TaskData>? taskData = <TaskData>[];
 
   HomeData({this.teamData, this.statusSummary, this.taskData});
 
   HomeData.fromJson(Map<String, dynamic> json) {
     if (json['teams'] != null) {
-      teamData = <TeamData>[];
-      json['teams'].forEach((value) {
-        teamData?.add(TeamData.fromJson(value));
-      });
+      teamData = (json['teams'] as List)
+          .map((value) => TeamData.fromJson(value))
+          .toList();
     }
     if (json['status_summary'] != null) {
-      statusSummary = <StatusSummary>[];
-      json['status_summary'].forEach((value) {
-        statusSummary?.add(StatusSummary.fromJson(value));
-      });
+      statusSummary = (json['status_summary'] as List)
+          .map((value) => StatusSummary.fromJson(value))
+          .toList();
     }
     if (json['tasks'] != null) {
-      taskData = <TaskData>[];
-      json['tasks'].forEach((value) {
-        taskData?.add(TaskData.fromJson(value));
-      });
+      taskData = (json['tasks'] as List)
+          .map((value) => TaskData.fromJson(value))
+          .toList();
     }
   }
 }
